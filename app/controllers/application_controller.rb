dde2914b729edf4e09ca04e6e6c74ebfa2fc8057
Parker <ygfert@gmail.com>
2020-10-28 13:53:13 +0800

    建立兌換表單

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+
+  private
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_token]) # if user_signed_in?
+  end
+  
 end
--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,6 +1,7 @@
 class CouponsController < ApplicationController
 
   def show
+    @user = current_user
   end
 
 end
